1c5c36a8a0028a29cd672ceba935bd4f8b63dd72
i31695 <Pallavi.Suthar@verisk.com>
2022-09-27 18:20:09 +0530

    Response

---
+++ WebApis/LookupApp/Lookup.Service/Models/LookupResponse.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Net;
+
+namespace Lookup.Service.Models
+{
+    public class LookupResponse
+    {
+        public string ErrorMessage { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string State { get; set; }
+    }
+}
